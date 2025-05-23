73d6d4edaceda3963d6ddcb3272acd841b23a69b
benjaminmehrez <benjaminjofre01@gmail.com>
2025-05-23 01:50:46 -0300

    update bestflowers

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -2,7 +2,7 @@ function Banner() {
   return (
     <section className="banner flex justify-center">
       <header className="absolute top-8 left-8 sm:left-10 text-white">
-        <h2 className="font-semibold text-xl">Floreria Madison</h2>
+        <h2 className="font-semibold text-xl sm:text-2xl">Floreria Madison</h2>
       </header>
       <div className="max-w-5xl mx-auto text-white flex flex-col items-center justify-center gap-4 px-5">
         <h3 className="mt-10 text-sm sm:text-lg font-medium">
@@ -16,8 +16,22 @@ function Banner() {
           fragancia y elegancia a tu vida. Descubre la expresión floral perfecta
           para cada momento y ocasión.
         </p>
-        <button className="btn sm:p-6 md:text-lg bg-pink-400 text-white border-none">
-          CONTACTANOS
+        <button className="relative inline-flex h-12 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)]"></span>
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg px-7 text-md font-bold text-white backdrop-blur-3xl gap-2 undefined">
+            CONTACTANOS
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 448 512"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z"></path>
+            </svg>
+          </span>
         </button>
       </div>
     </section>
--- frontend/src/components/BestFlowers.tsx
+++ frontend/src/components/BestFlowers.tsx
@@ -1,76 +1,171 @@
 function BestFlowers() {
   return (
-    <section className="bg-pink-950">
-      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+    <section className="bg-pink-100">
+      <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col items-center w-full mb-10">
           <div>
-            <h1 className="text-3xl font-medium title-font text-white mb-2">
+            <h1 className="text-3xl font-medium title-font mb-2">
               Ramos Destacados
             </h1>
             <div className="mx-auto w-1/4 h-1 bg-pink-500 mb-4" />
           </div>
-          <p className="mt-2 text-lg text-gray-100">
+          <p className="mt-2 text-lg">
             Elegimos los ramos más bellos y delicados para sorprender en cada
             ocasión.
           </p>
         </div>
-        <div className="flex flex-wrap md:-m-2 -m-1">
-          <div className="flex flex-wrap w-1/2">
-            <div className="md:p-2 p-1 w-1/2">
-              <img
-                alt="gallery"
-                className="w-full object-cover h-full object-center block rounded-lg"
-                src="/img1.jpg"
-                height={300}
-                width={500}
-              />
-            </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <img
-                alt="gallery"
-                className="w-full object-cover h-full object-center block rounded-lg"
-                src="/img2.jpg"
-                height={301}
-                width={501}
-              />
-            </div>
-            <div className="md:p-2 p-1 w-full">
-              <img
-                alt="gallery"
-                className="w-full object-cover h-full object-center block rounded-lg"
-                src="/img3.jpg"
-                height={360}
-                width={600}
-              />
+        <div className="flex justify-center flex-wrap gap-10">
+          <div className="group relative bg-[url(/img1.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 45.000,00</p>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="flex flex-wrap w-1/2">
-            <div className="md:p-2 p-1 w-full">
-              <img
-                alt="gallery"
-                className="w-full object-cover h-full object-center block rounded-lg"
-                src="/img4.jpg"
-                height={361}
-                width={601}
-              />
+
+          <div className="group relative bg-[url(/img2.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 50.000,00</p>
+                </div>
+              </div>
             </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <img
-                alt="gallery"
-                className="w-full object-cover h-full object-center block rounded-lg"
-                src="/img5.jpg"
-                height={302}
-                width={502}
-              />
+          </div>
+          <div className="group relative bg-[url(/img8.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 50.000,00</p>
+                </div>
+              </div>
             </div>
-            <div className="md:p-2 p-1 w-1/2">
-              <img
-                alt="gallery"
-                className="w-full object-cover h-full object-center block rounded-lg"
-                src="/img6.jpg"
-                height={303}
-                width={503}
-              />
+          </div>
+          <div className="group relative bg-[url(/img3.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 60.000,00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group relative bg-[url(/img4.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 80.000,00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group relative bg-[url(/img7.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 50.000,00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group relative bg-[url(/img5.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 20.000,00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group relative bg-[url(/img6.jpg)] bg-cover bg-center h-[450px] w-[300px] rounded-lg">
+            <div className="absolute inset-0 bg-transparent from-black/100 lg:from-black/0 lg:group-hover:from-black/100 bg-gradient-to-t bg-gradient-to-b rounded-lg transition duration-300"></div>
+
+            <div className="relative p-4 sm:p-6 lg:p-8">
+              <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                Flower
+              </p>
+
+              <p className="text-xl font-bold text-white sm:text-2xl">
+                Custom Floral Designs
+              </p>
+
+              <div className="mt-72 sm:mt-60 lg:mt-68">
+                <div className="translate-y-8 transform lg:opacity-0 transition-all lg:group-hover:translate-y-0 lg:group-hover:opacity-100">
+                  <p className="text-lg text-white font-bold">$ 30.000,00</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
