ae1c639697996c9688d32c28829fc6c926e1e835
benjaminmehrez <benjaminjofre01@gmail.com>
2025-06-13 11:38:56 -0300

    update load

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer')
+      ],
+    },
+  },
+  build: {
+    minify: 'esbuild',
   },
 })
